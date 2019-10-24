57876c0d1c4f310e12bbb4b9748dfd9505f393b6
Keegan Leary <leary.keegan@gmail.com>
2019-10-23 19:26:55 -0700

    deleted all the imports that broke stuff

--- app/hello.py
+++ app/hello.py
@@ -1,14 +1,6 @@
 from flask import Flask, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
-import sys
-import json
-from flask_heroku import Heroku
-
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-heroku = Heroku(app)
-db = SQLAlchemy(app)
 
 
 @app.route('/')
