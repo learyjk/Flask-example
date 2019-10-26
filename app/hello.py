ff49f091581eb0fe48e469eb2fc5b285816798ea
Keegan Leary <leary.keegan@gmail.com>
2019-10-25 22:19:17 -0700

    Added random functionality and card style.

--- app/hello.py
+++ app/hello.py
@@ -3,6 +3,7 @@ import sqlalchemy as db
 
 from flask import Flask, render_template
 from sqlalchemy.orm import scoped_session, sessionmaker
+from random import randint
 
 
 app = Flask(__name__)
@@ -15,15 +16,6 @@ if not os.getenv("DATABASE_URL"):
 engine = db.create_engine(os.getenv("DATABASE_URL"), echo=True)
 data = scoped_session(sessionmaker(bind=engine))
 
-#get row with id=MAKE IT RANDOM
-results = data.execute('SELECT * FROM wods WHERE id=1')
-row = results.fetchone()
-workout = []
-workout.append(row[1])
-exercises = row[2].split(',')
-for exercise in exercises:
-    workout.append(exercise)
-
 
 @app.route('/')
 def home():
@@ -32,7 +24,24 @@ def home():
 
 @app.route('/wodg')
 def wodg():
-    return render_template('wodg.html', results=results, workout=workout)
+
+    # get random int between 0 and num rows in workout database.
+    num_rows = data.execute('SELECT count(*) FROM wods').fetchall()[0][0]
+    workout_id = randint(1, num_rows)
+    results = data.execute(
+        'SELECT * FROM wods WHERE id=:workout_id', {'workout_id': workout_id})
+
+    # fetch the workout
+    row = results.fetchone()
+
+    # creates a row with name as workout[1], type as [2], and the rest.
+    workout = []
+    workout.append(row[1])
+    exercises = row[2].split(',')
+    for exercise in exercises:
+        workout.append(exercise)
+
+    return render_template('wodg.html', workout=workout)
 
 
 if __name__ == '__main__':
