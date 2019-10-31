b811e971225eb2b8dcb58ec9eec79397cc116e5d
Keegan Leary <leary.keegan@gmail.com>
2019-10-30 23:11:33 -0700

    comment out DBURL

--- app/hello.py
+++ app/hello.py
@@ -19,7 +19,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-os.environ['DATABASE_URL'] = postgres://sbcrjibpraivpg:cac3d5eaf635b550f67cf802268437d7f2dd6eccf84a13506ae52c5be32f53e4@ec2-107-21-200-103.compute-1.amazonaws.com:5432/d4keqqkhpmbhkn
+#os.environ['DATABASE_URL'] = postgres://sbcrjibpraivpg:cac3d5eaf635b550f67cf802268437d7f2dd6eccf84a13506ae52c5be32f53e4@ec2-107-21-200-103.compute-1.amazonaws.com:5432/d4keqqkhpmbhkn
 os.environ['SECRET_KEY'] = '3r6t_QQHDSCMtGGjbBYuQQ'
 
 # Check for environment variable
